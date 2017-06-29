4da61f8e90c30a9261ebbe37be96d8ac0745c048
Nelson Atle <nelson.atle@gmail.com>
2017-06-29 13:25:25 -0600

    Pagina Principal

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,7 @@
 $(document).ready(main);
 
-var contador = 1;//Nos va funcionar para validar si el menu esta activo o no
-
 function main(){
-    $('#menu-bar').click(function(){
-        $('#divPerfil').toggle(500);
-    });
+        $('#menu-bar').on('click',function(){
+            $('#divPerfil').toggle(500);
+        });
 };
